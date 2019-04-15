2f1b1b9e4ccc1384b8631154f317f64de401a70d
nicruo <nicruo@gmail.com>
2019-04-15 21:48:29 +0100

    Added MiniGame state machine;

---
+++ Assets/MiniGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGame : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (animatorStateInfo.IsTag("garden"))
+        {
+            Debug.Log("Enter garden");
+        }
+    }
+}
